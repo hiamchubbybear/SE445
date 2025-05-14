634e0f627ba9d12eba11b31ab46aee272573022d
nvmthoai <nvmthoai12@gmail.com>
2025-05-14 19:58:07 +0700

    fix bug

--- Server/src/index.js
+++ Server/src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const router = require("./routes/web.js");
 const app = express();
+const cors = require("cors");
 const ejs = require("ejs");
 const port = process.env.PORT;
 const CONNECTION_STRING = process.env.MONGODB_URI;
@@ -9,7 +10,9 @@ const path = require("path");
 const configViewEngine = require("./config/configViewEngine.js");
 app.use(express.json());
 configViewEngine(app);
-app.use("v1", router);
+app.use(cors());
+app.use(express.json());
+app.use("/v1", router);
 console.log(CONNECTION_STRING);
 
 app.listen(port, () => {
